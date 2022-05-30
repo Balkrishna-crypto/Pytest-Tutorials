4b58873ff2c4b8075504a1075dc820f8d787fdb1
Balkrishna-QA <boli@unifiedcommunications.com>
2022-05-30 17:03:42 +0545

    a

---
+++ test_fixture.py
@@ -0,0 +1,24 @@
+
+import pytest
+
+@pytest.fixture
+# Whenever we need to run some code before test we use fixture.
+
+def nums():
+    a=5
+    b=7
+    c=10
+    return[a,b,c]
+
+def test_method1(nums):
+    x=5
+    assert nums[0]==x
+
+def test_method2(nums):
+    x=8
+    assert nums[2]==x
+
+def test_method3(nums):
+    x=7
+    assert nums[1]==x
+
